6317b772d7a067b7d9ba4c2d8bd1fdb04d3afe31
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-08-27 14:56:49 +0100

    testing

--- flake.nix
+++ flake.nix
@@ -1,154 +1,51 @@
 {
-  description = "Ready-made templates for easily creating flake-driven environments";
+  description = "My personal flake templates";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs =
-    inputs:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import inputs.nixpkgs { inherit system; };
-          }
-        );
+  outputs = { self, nixpkgs }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
 
-      scriptDrvs = forEachSupportedSystem (
-        { pkgs }:
-        let
-          getSystem = "SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')";
-          forEachDir = exec: ''
-            for dir in */; do
-              (
-                cd "''${dir}"
-
-                ${exec}
-              )
-            done
-          '';
-        in
-        {
-          format = pkgs.writeShellApplication {
-            name = "format";
-            runtimeInputs = with pkgs; [ nixfmt-rfc-style ];
-            text = ''
-              git ls-files '**/*.nix' | xargs nix fmt
-            '';
-          };
-
-          # only run this locally, as Actions will run out of disk space
-          build = pkgs.writeShellApplication {
-            name = "build";
-            text = ''
-              ${getSystem}
-
-              ${forEachDir ''
-                echo "building ''${dir}"
-                nix build ".#devShells.''${SYSTEM}.default"
-              ''}
-            '';
-          };
-
-          check = pkgs.writeShellApplication {
-            name = "check";
-            text = forEachDir ''
-              echo "checking ''${dir}"
-              nix flake check --all-systems --no-build
-            '';
-          };
-
-          update = pkgs.writeShellApplication {
-            name = "update";
-            text = forEachDir ''
-              echo "updating ''${dir}"
-              nix flake update
-            '';
-          };
-        }
-      );
-    in
-    {
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages =
-              with scriptDrvs.${pkgs.system};
-              [
-                build
-                check
-                format
-                update
-              ]
-              ++ [ pkgs.nixfmt-rfc-style ];
-          };
-        }
-      );
-
-      formatter = forEachSupportedSystem ({ pkgs }: pkgs.nixfmt-rfc-style);
-
-      packages = forEachSupportedSystem (
-        { pkgs }:
-        rec {
-          default = dvt;
-          dvt = pkgs.writeShellApplication {
-            name = "dvt";
-            bashOptions = [
-              "errexit"
-              "pipefail"
-            ];
-            text = ''
-              if [ -z "''${1}" ]; then
-                echo "no template specified"
-                exit 1
-              fi
-
-              TEMPLATE=$1
-
-              nix \
-                --experimental-features 'nix-command flakes' \
-                flake init \
-                --template \
-                "https://flakehub.com/f/the-nix-way/dev-templates/0.1#''${TEMPLATE}"
-            '';
-          };
-        }
-      );
-    }
-
-    //
-
-      {
-        templates = rec {
-          default = empty;
-
-          empty = {
-            path = ./empty;
-            description = "default";
-          };
-
-          c = {
-            path = ./c;
-            description = "C/C++";
-          };
-
-          java = {
-            path = ./java;
-            description = "Java";
-          };
-
-          latex = {
-            path = ./latex;
-            description = "LaTeX";
-          };
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems
+        (system: f { inherit system; });
+  in {
+    devShells = forEachSupportedSystem ({ system }:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          packages = [ pkgs.nixfmt-rfc-style ];
         };
+      });
+
+    formatter = forEachSupportedSystem ({ system }:
+      import nixpkgs { inherit system; }).nixfmt-rfc-style;
+
+    templates = {
+      default = {
+        path = ./empty;
+        description = "Empty starter template";
       };
+
+      c = {
+        path = ./c;
+        description = "C/C++ project template";
+      };
+
+      java = {
+        path = ./java;
+        description = "Java project template";
+      };
+
+      latex = {
+        path = ./latex;
+        description = "LaTeX project template";
+      };
+    };
+  };
 }
