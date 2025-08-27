92e7c86d06fe90a465a5348fa224a64b5ab74485
Ahmed Hamzaoui <lazyfiki@gmail.com>
2025-08-27 14:38:26 +0100

    initial commit

---
+++ c/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "C/C++ development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+      {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                clang-tools
+                cmake
+                codespell
+                conan
+                cppcheck
+                doxygen
+                gtest
+                lcov
+                vcpkg
+                vcpkg-tool
+              ]
+              ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+
+            env = {
+              LC_MESSAGES = "en_US.UTF-8";
+            };
+          };
+        }
+      );
+    };
+}
---
+++ flake.nix
@@ -0,0 +1,139 @@
+{
+  description = "Ready-made templates for easily creating flake-driven environments";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+
+      scriptDrvs = forEachSupportedSystem (
+        { pkgs }:
+        let
+          getSystem = "SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')";
+          forEachDir = exec: ''
+            for dir in */; do
+              (
+                cd "''${dir}"
+
+                ${exec}
+              )
+            done
+          '';
+        in
+        {
+          format = pkgs.writeShellApplication {
+            name = "format";
+            runtimeInputs = with pkgs; [ nixfmt-rfc-style ];
+            text = ''
+              git ls-files '**/*.nix' | xargs nix fmt
+            '';
+          };
+
+          # only run this locally, as Actions will run out of disk space
+          build = pkgs.writeShellApplication {
+            name = "build";
+            text = ''
+              ${getSystem}
+
+              ${forEachDir ''
+                echo "building ''${dir}"
+                nix build ".#devShells.''${SYSTEM}.default"
+              ''}
+            '';
+          };
+
+          check = pkgs.writeShellApplication {
+            name = "check";
+            text = forEachDir ''
+              echo "checking ''${dir}"
+              nix flake check --all-systems --no-build
+            '';
+          };
+
+          update = pkgs.writeShellApplication {
+            name = "update";
+            text = forEachDir ''
+              echo "updating ''${dir}"
+              nix flake update
+            '';
+          };
+        }
+      );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages =
+              with scriptDrvs.${pkgs.system};
+              [
+                build
+                check
+                format
+                update
+              ]
+              ++ [ pkgs.nixfmt-rfc-style ];
+          };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs }: pkgs.nixfmt-rfc-style);
+
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        rec {
+          default = dvt;
+          dvt = pkgs.writeShellApplication {
+            name = "dvt";
+            bashOptions = [
+              "errexit"
+              "pipefail"
+            ];
+            text = ''
+              if [ -z "''${1}" ]; then
+                echo "no template specified"
+                exit 1
+              fi
+
+              TEMPLATE=$1
+
+              nix \
+                --experimental-features 'nix-command flakes' \
+                flake init \
+                --template \
+                "https://flakehub.com/f/the-nix-way/dev-templates/0.1#''${TEMPLATE}"
+            '';
+          };
+        }
+      );
+    }
+
+    //
+
+      {
+        templates = rec {
+          default = empty;
+
+          empty = {
+            path = ./empty;
+            description = "default";
+          };
+        };
+      };
+}
